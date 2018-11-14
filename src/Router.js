d6cc4513a17e8105ec60ab78c4603f432da50bee
KpStar <kpstar.mc@gmail.com>
2018-11-14 13:21:24 +0200

    CC003 - Update npm packages for react native and initial router.js

---
+++ src/Router.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+import { StackNavigator } from 'react-navigation';
+
+// import Login from "./screens/Login"
+// import Splash from "./screens/Splash"
+// import Parent from "./screens/LoginScreen/Parent"
+// import Children from "./screens/LoginScreen/Children"
+// import PhoneNumber from "./screens/VerificationScreen/PhoneNumber"
+// import EmailScreen from "./screens/VerificationScreen/EmailVerification"
+// import ChildrenInfo from "./screens/InfoScreen/ChildrenInfo"
+// import ParentInfo from "./screens/InfoScreen/ParentInfo"
+// import MapScreen from "./screens/MainScreen/MapScreen"
+// import ParentQRCode from "./screens/InfoScreen/ParentQRCode"
+// import ChildrenQR from "./screens/InfoScreen/ChildrenQR"
+
+export const PrimaryNav = StackNavigator({
+    
+    // SplashScreen: {screen: Splash},
+    // LoginScreen: {screen: Login},
+    // ParentLoginScreen: {screen: Parent},
+    // ChildrenLoginScreen: {screen: Children},
+    // PhoneScreen: {screen: PhoneNumber},
+    // EmailScreen: {screen: EmailScreen},
+    // ChildInfoScreen: {screen: ChildrenInfo},
+    // ParentInfoScreen: {screen: ParentInfo},
+    // MapScreen: {screen: MapScreen},
+    // ParentQRScreen: {screen: ParentQRCode},
+    // ChildrenQRScreen: {screen: ChildrenQR},
+}, {
+    headerMode: 'none',
+})
